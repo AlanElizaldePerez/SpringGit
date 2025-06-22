bcdf5d6614bc5c3e2e9a20e8aff424f15286363f
AlanElizaldePerez <elizaldeperezalan@gmail.com>
2025-06-22 14:17:53 -0600

    PrimerCommit

---
+++ BaseH2_Proy/src/main/java/com/example/ejercicioSpring/BaseH2ProyApplication.java
@@ -0,0 +1,13 @@
+package com.example.ejercicioSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseH2ProyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseH2ProyApplication.class, args);
+	}
+
+}
